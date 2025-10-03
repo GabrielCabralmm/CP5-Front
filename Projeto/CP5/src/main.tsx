7dcdb98a2068d7868155f21d7a04db7922e9f395
GabrielCabralmm <gabriel.cmmariano@gmail.com>
2025-10-03 20:07:46 -0300

    Limpando o projeto e organizando os elementos principais.

--- Projeto/CP5/src/main.tsx
+++ Projeto/CP5/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import "./global.css";
+
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {path:"/", element:<App/>, errorElement:<Error/> , children:[
+    {path:"/",element:<Home/>}
+  ]}
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
