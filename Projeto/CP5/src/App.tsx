e8eaae52e4d481a956b72b2d9af1a63f6e26d8dd
EnzoMMaciel10 <enzomontmaciel@gmail.com>
2025-10-06 23:41:26 -0300

    App atualizado, integra Cabecalho e Rodape no layout do App

--- Projeto/CP5/src/App.tsx
+++ Projeto/CP5/src/App.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
 
-export default function App(){
-
-  return(
-    <div className="container">
-        <Outlet/>
+export default function App() {
+  return (
+    <div className="min-h-dvh flex flex-col">
+      <Cabecalho />
+      <main className="container grow">
+        <Outlet />
+      </main>
+      <Rodape />
     </div>
   );
 }
