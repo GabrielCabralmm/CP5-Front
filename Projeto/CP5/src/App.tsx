cbfe78a2a6b00733d31f44abc13f952ae856749d
EnzoMMaciel10 <enzomontmaciel@gmail.com>
2025-10-07 18:33:22 -0300

    realizei melhorias no funcionamento e adicionei um botão "sair" para limpar o localStorage.

--- Projeto/CP5/src/App.tsx
+++ Projeto/CP5/src/App.tsx
@@ -1,10 +1,26 @@
+
 import { Outlet } from "react-router-dom";
+import { useAuth } from "./AuthContext";
 
 export default function App(){
+  const { usuario, setUsuario } = useAuth();
 
-  return(
+  function sair(){
+    localStorage.removeItem("usuarioLogado");
+    setUsuario(null);
+  }
+
+  return (
     <div className="container">
-        <Outlet/>
+      <header className="p-4 text-sm flex gap-4 items-center">
+        {usuario ? (
+          <>
+            <p>Logado como: <strong>{usuario.nome}</strong> &lt;{usuario.email}&gt;</p>
+            <button onClick={sair} className="text-blue-700 underline">Sair</button>
+          </>
+        ) : <p>Não autenticado</p>}
+      </header>
+      <Outlet/>
     </div>
   );
 }
